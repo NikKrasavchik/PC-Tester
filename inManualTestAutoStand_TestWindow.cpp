40d81eef7dd4c31e5b39bb120c38f69b1f2b18d1
Стачинский Никита <stachinskiy.nikita@gmail.com>
2025-03-24 15:37:15 +0300

    Исправлена ошибка, при которой неправильно устанавливалась высота строки в TestWindow

--- inManualTestAutoStand_TestWindow.cpp
+++ inManualTestAutoStand_TestWindow.cpp
@@ -149,7 +149,8 @@ void TestWindow::initUiTableRowsInManualTestAutoStand()
 	QAbstractItemModel* model = mainTableWidget->model();
 	for (int currentRowNum = 0; currentRowNum < cableRows.size(); currentRowNum++)
 	{
-		mainTableWidget->setRowHeight(currentRowNum, MIN_ROW_HEIGHT);
+		if (mainTableWidget->rowHeight(currentRowNum) < MIN_ROW_HEIGHT)
+			mainTableWidget->setRowHeight(currentRowNum, MIN_ROW_HEIGHT);
 		model->setData(model->index(currentRowNum, COLUMN_CONNECTOR), cableRows[currentRowNum]->connectorStr + "\nXP" + QString::number((int)cableRows[currentRowNum]->connectorInt));
 		model->setData(model->index(currentRowNum, COLUMN_PIN), cableRows[currentRowNum]->pin);
 		model->setData(model->index(currentRowNum, COLUMN_NAME), cableRows[currentRowNum]->name);
